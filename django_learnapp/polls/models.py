fe8afe3837ed5d069c90eaae01f7ab9bc3420257
Juan Acosta <juank_mate@hotmail.com>
2023-03-20 15:26:46 -0500

    Se inicializa una app con su ORM, object relational mapping

--- django_learnapp/polls/models.py
+++ django_learnapp/polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+"""
+La llave primaria ya se genera con la herencia
+
+Para inicializar una app:
+----> py manage.py startapp nombre_de_tu_aplicacion
+y seguido, debe hacerse el ORM, agregar la app a INSTALED_APPS
+de settings, y ejecutar:
+----> py manage.py makemigrations nombre_de_tu_aplicacion
+y despues:
+----> py manage.py migrate
+
+"""
+
+class Question(models.Model):
+    
+    question_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('date published')
+
+class Choices(models.Model):
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=300)
+    votes = models.IntegerField(default=0)
