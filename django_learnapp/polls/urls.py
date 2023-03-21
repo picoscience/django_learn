7ecc30738b33c1e3e1ced8c4713029547ba7b7f3
Juan Acosta <juank_mate@hotmail.com>
2023-03-20 19:24:22 -0500

    Se agrego una view de la aplicacion polls, y también se agregaron los paths al archivo urls.py

--- django_learnapp/polls/urls.py
+++ django_learnapp/polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path("", views.index, name="index"),
+    path("detail/<int:question_id>", views.detail, name="detail"),
+    path("results/<int:question_id>", views.results, name="results"),
+    path("vote/<int:question_id>", views.vote, name="vote"),
 ]
--- django_learnapp/polls/views.py
+++ django_learnapp/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 
 def index(request):
-    return HttpResponse('Hola mundo')
+    return HttpResponse('You are in the Index page')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are seeing the Question number {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are seeing the results to Question number {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting to Question number {question_id}')
